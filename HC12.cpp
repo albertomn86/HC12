4006c6dcbf7e869d258b66a3b67039e396da4a18
Alberto Marín <albertomn@outlook.com>
2020-04-30 18:16:54 +0200

    Add new functions

--- HC12.cpp
+++ HC12.cpp
@@ -1,12 +1,18 @@
 #include "HC12.hpp"
 
+unsigned long AT_DELAY = 100;
+
 /**
  * @brief Construct a new HC12 object.
  *
  * @param RX_Pin RXD pin on HC12 module.
  * @param TX_Pin TXD pin on HC12 module.
  */
-HC12::HC12(uint8_t RX_Pin, uint8_t TX_Pin): _serial(SoftwareSerial(RX_Pin, TX_Pin)), _SET_PIN(0) {}
+HC12::HC12(uint8_t RX_Pin, uint8_t TX_Pin): _serial(SoftwareSerial(RX_Pin, TX_Pin))
+{
+    _SET_PIN = 0;
+    _MODE = FU3;
+}
 
 
 /**
@@ -16,15 +22,19 @@ HC12::HC12(uint8_t RX_Pin, uint8_t TX_Pin): _serial(SoftwareSerial(RX_Pin, TX_Pi
  * @param TX_Pin TXD pin on HC12 module.
  * @param SET_Pin SET pin on HC12 module.
  */
-HC12::HC12(uint8_t RX_Pin, uint8_t TX_Pin, uint8_t SET_Pin): _serial(SoftwareSerial(RX_Pin, TX_Pin)), _SET_PIN(SET_Pin) {}
+HC12::HC12(uint8_t RX_Pin, uint8_t TX_Pin, uint8_t SET_Pin): _serial(SoftwareSerial(RX_Pin, TX_Pin))
+{
+    _SET_PIN = SET_Pin;
+    _MODE = FU3;
+}
 
 
 /**
  * @brief Initialize the communication with the HC12 module.
  */
-void HC12::begin(void)
+void HC12::begin(int baudrate)
 {
-    _serial.begin(9600);
+    _serial.begin(baudrate);
     if (_SET_PIN > 0)
     {
         digitalWrite(_SET_PIN, HIGH);
@@ -37,9 +47,9 @@ void HC12::begin(void)
  * @brief Sleep mode.
  *
  */
-void HC12::sleep(void) {
+void HC12::sleep(void)
+{
     if (_SET_PIN == 0) return;
-
     digitalWrite(_SET_PIN, LOW);
     delay(AT_DELAY);
     // Send command
@@ -59,9 +69,9 @@ void HC12::sleep(void) {
  * @brief Wake Up.
  *
  */
-void HC12::wakeup(void) {
+void HC12::wakeup(void)
+{
     if (_SET_PIN == 0) return;
-
     digitalWrite(_SET_PIN, LOW);
     delay(AT_DELAY);
     // Send command
@@ -78,15 +88,17 @@ void HC12::wakeup(void) {
 
 
 /**
- * @brief Change serial port transparent transmission mode.
+ * @brief Sent AT command to HC-12 module.
  *
- * @param mode HC12 mode.
+ * @param command Command
  */
-void HC12::setup_mode(Mode mode) {
+void HC12::send_AT(String command)
+{
+    if (_SET_PIN == 0) return;
     digitalWrite(_SET_PIN, LOW);
     delay(AT_DELAY);
     // Send command
-    _serial.print("AT+FU" + mode);
+    _serial.print(command);
     delay(AT_DELAY);
     // Read response
     while(_serial.available() > 0) {
@@ -98,20 +110,106 @@ void HC12::setup_mode(Mode mode) {
 
 
 /**
+ * @brief Change serial port transparent transmission mode.
+ *
+ * @param mode HC12 mode.
+ */
+void HC12::set_mode(Mode mode)
+{
+    HC12::send_AT("AT+FU" + mode);
+}
+
+
+/**
  * @brief Set transmitting power of module.
  *
  * @param power Power value from 1 to 8.
  */
-void HC12::setup_power(short power) {
-    digitalWrite(_SET_PIN, LOW);
-    delay(AT_DELAY);
-    // Send command
-    _serial.print("AT+P" + String(power));
-    delay(AT_DELAY);
-    // Read response
-    while(_serial.available() > 0) {
-        char c = _serial.read();
+void HC12::set_power(short power)
+{
+    HC12::send_AT("AT+P" + String(power));
+}
+
+
+/**
+ * @brief Set wireless communication channel.
+ *
+ * @param channel Channel number from 001 to 127.
+ */
+void HC12::set_channel(String channel)
+{
+    HC12::send_AT("AT+C" + channel);
+}
+
+
+/**
+ * @brief Set serial port baud rate.
+ *
+ * @param baudrate Serial baudrate.
+ * The baud rate can be set to be 1,200bps, 2,400bps, 4,800bps,
+ * 9,600bps, 19,200bps, 38,400bps, 57,600bps, and 115,200bps.
+ */
+void HC12::set_baudrate(unsigned int baudrate)
+{
+    HC12::send_AT("AT+B" + String(baudrate));
+}
+
+
+/**
+ * @brief Send message.
+ *
+ * @param message Message to be sent.
+ */
+void HC12::send(String message)
+{
+    _serial.print(message);
+    // Wait for sending.
+    unsigned long send_delay = message.length();
+    switch (_MODE)
+    {
+        case FU1:  // Transmision data delay is 15-25 ms/byte.
+        send_delay *= 25;
+        break;
+
+        case FU2:  // Transmision data delay is 500 ms/byte.
+        send_delay *= 500;
+        break;
+
+        case FU3:  // Transmision data delay is 4-80 ms/byte.
+        send_delay *= 80;
+        break;
     }
-    digitalWrite(_SET_PIN, HIGH);
-    delay(AT_DELAY);
+    delay(send_delay);
+}
+
+
+/**
+ * @brief Receive message.
+ *
+ * @param timeout Timeout. Min allowed value: 1000 ms.
+ * @param end End line character.
+ * @return Message.
+ */
+String HC12::receive(unsigned int timeout, char end)
+{
+
+    if (timeout < 1000) timeout = 1000;
+    while(!_serial.available() && timeout > 0) {
+        delay(50);
+        timeout -= 50;
+    }
+
+    if (!timeout) {
+        return;
+    }
+
+    String msg;
+    char byte;
+    _serial.setTimeout(1000);
+    while(_serial.available() && byte != end) {
+        byte = char(_serial.read());
+        msg += byte;
+    }
+
+    return msg;
 }
--- HC12.hpp
+++ HC12.hpp
@@ -1,5 +1,5 @@
 /**
- *  @file HC-12.hpp
+ *  @file HC12.hpp
  *  @author Alberto MN
  *
  *  This is a library for long-distance wireless HC-12 modules.
@@ -9,14 +9,8 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#define AT_DELAY 100
-
 class HC12
 {
-    private:
-        SoftwareSerial _serial;
-        uint8_t _SET_PIN;
-
     public:
         HC12(uint8_t RX_Pin, uint8_t TX_Pin);
         HC12(uint8_t RX_Pin, uint8_t TX_Pin, uint8_t SET_Pin);
@@ -26,11 +20,21 @@ class HC12
             FU2 = 50,
             FU3 = 51
         };
-        void begin(void);
+        void begin(int baudrate);
         void sleep(void);
         void wakeup(void);
-        void setup_mode(Mode mode);
-        void setup_power(short power);
+        void set_mode(Mode mode);
+        void set_power(short power);
+        void set_channel(String channel);
+        void set_baudrate(unsigned int baudrate);
+        void send(String message);
+        String receive(unsigned int timeout, char end);
+
+    private:
+        SoftwareSerial _serial;
+        uint8_t _SET_PIN;
+        Mode _MODE;
+        void send_AT(String command);
 };
 
 #endif
